efe40266495183ffeb879829bab63c5509302a50
Niels Lucas <niels_lucas@hotmail.com>
2017-02-03 10:55:58 +0100

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            string text = "something";
+            Console.WriteLine("{0} {1}", number, text);
+            Console.ReadLine();
+        }
+    }
+}
