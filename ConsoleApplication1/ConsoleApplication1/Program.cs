aa0b2e2af13c6ce74f4e1ffbd0a419a8ffe89aaf
Niels Lucas <niels_lucas@hotmail.com>
2017-02-03 11:26:35 +0100

    Calcuation

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,9 +10,17 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int number = 5;
-            string text = "something";
-            Console.WriteLine("{0} {1}", number, text);
+            int number1; // declare first number to add 
+            int number2; // declare second number to add 
+            int sum; // declare sum of number1 and number2
+
+            Console.Write("Enter first integer: "); // prompt user 15
+            number1 = Convert.ToInt32( Console.ReadLine() );
+            Console.Write( "Enter second integer: " ); // prompt user
+            number2 = Convert.ToInt32(Console.ReadLine());
+            sum = number1 + number2; // add numbers
+            Console.WriteLine("Sum is {0}", sum); // display sum
+
 
             Console.ReadLine();
         }
