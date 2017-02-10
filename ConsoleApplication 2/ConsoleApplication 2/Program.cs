31808c60e49dac28238f00846004fca89d1274ca
Niels Lucas <niels_lucas@hotmail.com>
2017-02-10 10:53:01 +0100

    Create a Gradebook with a name you type

---
+++ ConsoleApplication 2/ConsoleApplication 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_2
+{
+    class GradeBook
+    {
+        //static void Main(string[] args)
+        //{
+        //    GradeBook myGradeBook = new GradeBook();
+        //    myGradeBook.DisplayMessage("Niels");
+        //}
+
+        public void DisplayMessage(string courseName)
+        {
+            Console.WriteLine("Welcome to the Grade Book for {0}!", courseName);
+            Console.ReadLine();
+        }
+    }
+
+    class GradeBookTest
+    {
+        public static void Main(string[] args)
+        {
+            GradeBook myGradeBook = new GradeBook();
+
+            Console.WriteLine("Please enter your gradebook name");
+            string nameOfCourse = Console.ReadLine();
+            Console.WriteLine();
+
+            myGradeBook.DisplayMessage(nameOfCourse);
+        }
+    }
+}
--- ConsoleApplication 2/ConsoleApplication 2/Properties/AssemblyInfo.cs
+++ ConsoleApplication 2/ConsoleApplication 2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Unknown")]
+[assembly: AssemblyTitle("ConsoleApplication 2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Unknown")]
+[assembly: AssemblyProduct("ConsoleApplication 2")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("17658d72-1307-41a7-8921-2fcdd5ba87fc")]
+[assembly: Guid("18531c8d-e89a-4651-8418-573eb4171dd7")]
 
 // Version information for an assembly consists of the following four values:
 //
